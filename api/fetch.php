c37543ff589258802e46b9e92f277fb8c1f1cda6
Prashant Gupta <prashantguptagzp2708@gmail.com>
2026-04-18 23:40:27 +0530

    Implemented basic demand forecasting logic

--- api/fetch.php
+++ api/fetch.php
@@ -1,5 +1,17 @@
 <?php
 header('Content-Type: application/json');
+function calculateForecast($sales_history) {
+    if (empty($sales_history)) return 0;
+    return round(array_sum($sales_history) / count($sales_history), 2);
+}
+
 $data = json_decode(file_get_contents('../data.json'), true);
-echo json_encode($data['inventory']);
+$inventory = $data['inventory'];
+
+// Add forecast to each item
+foreach ($inventory as &$item) {
+    $item['forecast'] = isset($item['sales_history']) ? calculateForecast($item['sales_history']) : 0;
+}
+
+echo json_encode($inventory);
 ?>
